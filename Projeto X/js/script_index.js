e3d995189ecc74e2d7e65442cad9008afd5cf649
kleber maia <42123079+klebermmaia@users.noreply.github.com>
2021-06-02 17:10:57 -0300

    correções

---
+++ Projeto X/js/script_index.js
@@ -0,0 +1,14 @@
+
+
+const titulo = document.querySelector('h1');
+typeWrite(titulo);
+
+function typeWrite(elemento){
+const textoArray = elemento.innerHTML.split('');
+elemento.innerHTML = '';
+textoArray.forEach((letra, i) => {
+    setTimeout(() => elemento.innerHTML += letra, 80 * i)
+
+});
+
+}
